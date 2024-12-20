330dede5da37eb11009cf76214020ac97695312f
Douglas Chimento <dchimento@gmail.com>
2024-12-20 19:52:57 +0200

    token tests

--- tests/parseAssociated.test.ts
+++ tests/parseAssociated.test.ts
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import { AssociatedTokenProgramIdlLike, ParsedIdlInstruction, SolanaParser } from "../src/index";
 import { TestUtils } from "./utils";
-import { createAssociatedTokenAccountInstruction } from "@solana/spl-token";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction } from "@solana/spl-token";
 
 const parser = new SolanaParser([]);
 const pk = TestUtils.pk();
@@ -11,6 +11,8 @@ describe("parse associated account", () => {
 		const inst = createAssociatedTokenAccountInstruction(pk, pk, pk, pk);
 		const result = parser.parseInstruction(inst) as ParsedIdlInstruction<AssociatedTokenProgramIdlLike, "createAssociatedTokenAccount">;
 		assert.isDefined(result);
+		assert.equal(result.name, "createAssociatedTokenAccount");
+		assert.equal(result.programId.toBase58(), ASSOCIATED_TOKEN_PROGRAM_ID.toBase58());
 		assert.equal(result.accounts[0].pubkey, pk);
 	});
 });
