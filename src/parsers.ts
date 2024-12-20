6e2d97fce03b11a698bb72d1a76f9717d7846e49
Douglas Chimento <dchimento@gmail.com>
2024-12-20 12:12:09 +0200

    upgrade eslint

--- src/parsers.ts
+++ src/parsers.ts
@@ -12,7 +12,6 @@ import {
 	LoadedAddresses,
 	VersionedTransaction,
 	AddressLookupTableAccount,
-	AccountInfo,
 	StakeProgram,
 } from "@solana/web3.js";
 import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
