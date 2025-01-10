8d15a57897e82e0cefcf4bc805124aa287fe470c
Douglas Chimento <dchimento@gmail.com>
2025-01-10 15:33:56 +0200

    export SplToken22 idl

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./parsers";
 export * from "./helpers";
 export * from "./interfaces";
+export { SplToken22 } from "./programs/spl-token-22.program";
 export { convertLegacyIdlToV30 } from "./legacy.idl.converter";
